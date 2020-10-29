e406b2a05b8356d5e973bedf3c8e7b45cb432ed9
Anton Shabouta <zloyusr@gmail.com>
2020-10-29 10:07:32 +0300

    Added default policy (#5)

--- src/Retry/RetryMiddleware.php
+++ src/Retry/RetryMiddleware.php
@@ -6,19 +6,27 @@ namespace Onliner\CommandBus\Retry;
 
 use Onliner\CommandBus\Context;
 use Onliner\CommandBus\Middleware;
+use Throwable;
 
 final class RetryMiddleware implements Middleware
 {
+    /**
+     * @var Policy
+     */
+    private $default;
+
     /**
      * @var array<string, Policy>
      */
     private $policies;
 
     /**
+     * @param Policy   $default
      * @param Policy[] $policies
      */
-    public function __construct(array $policies)
+    public function __construct(Policy $default, array $policies)
     {
+        $this->default  = $default;
         $this->policies = $policies;
     }
 
@@ -29,11 +37,8 @@ final class RetryMiddleware implements Middleware
     {
         try {
             $next($message, $context);
-        } catch (\Throwable $error) {
-            if (!$policy = $this->policy($message)) {
-                throw $error;
-            }
-
+        } catch (Throwable $error) {
+            $policy = $this->policy($message);
             $policy->retry($message, $context, $error);
         }
     }
@@ -41,10 +46,10 @@ final class RetryMiddleware implements Middleware
     /**
      * @param object $message
      *
-     * @return Policy|null
+     * @return Policy
      */
-    private function policy(object $message): ?Policy
+    private function policy(object $message): Policy
     {
-        return $this->policies[get_class($message)] ?? null;
+        return $this->policies[get_class($message)] ?? $this->default;
     }
 }
