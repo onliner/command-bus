1c112d8c69eb7be5131a85caa399385a34d2ada9
Anton Shabouta <zloyusr@gmail.com>
2021-05-18 13:21:57 +0300

    Resolve handler for parent classes (#13)

--- src/Exception/UnknownHandlerException.php
+++ src/Exception/UnknownHandlerException.php
@@ -6,4 +6,8 @@ namespace Onliner\CommandBus\Exception;
 
 final class UnknownHandlerException extends CommandBusException
 {
+    public static function forCommand(object $command): self
+    {
+        return new self(sprintf('Handler for command "%s" not found.', get_class($command)));
+    }
 }
